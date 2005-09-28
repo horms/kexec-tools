1366c25d9cbcc14f32c4ffb393e365e27690f03e
Vivek Goyal <vgoyal@in.ibm.com>
2005-09-28 20:53:24 +0530

    kdump-kexec-tools: x86 elf32-core-header option fix
    o Problem: If --elf32-core-headers option is specified after --initrd option
      then initrd effectively becomes null and capture kernel does not see
      any initrd.
    
    o This problem happens because OPT_ELF32_CORE and OPT_RAMDISK are having
      same value. This problem got introduced when option --elf32-core-headers
      was moved from loader option to architecture dependent option.
    
    o This patch needs to be applied after applying consolidated kdump patch.
    
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Maneesh Soni <maneesh@in.ibm.com>

--- kexec/arch/i386/include/arch/options.h
+++ kexec/arch/i386/include/arch/options.h
@@ -6,9 +6,9 @@
 #define OPT_SERIAL_BAUD    (OPT_MAX+2)
 #define OPT_CONSOLE_VGA    (OPT_MAX+3)
 #define OPT_CONSOLE_SERIAL (OPT_MAX+4)
-#define OPT_ARCH_MAX       (OPT_MAX+5)
-#define OPT_ELF32_CORE     (OPT_MAX+6)
-#define OPT_ELF64_CORE     (OPT_MAX+7)
+#define OPT_ELF32_CORE     (OPT_MAX+5)
+#define OPT_ELF64_CORE     (OPT_MAX+6)
+#define OPT_ARCH_MAX       (OPT_MAX+7)
 
 #define KEXEC_ARCH_OPTIONS \
 	KEXEC_OPTIONS \
