horms/kexec-tools

commit 2b0088bb881680c5e8b062a9b0f0a6ef1b88d52c

kexec-tools: [arm] use proc_iomem()
Use proc_iomem() on arm for consistency with other architectures.

Signed-off-by: Simon Horman <horms@verge.net.au>

kexec/arch/arm/kexec-arm.c
@@ -26,7 +26,7 @@ static struct memory_range memory_range[MAX_MEMORY_RANGES];
 int get_memory_ranges(struct memory_range **range, int *ranges,
 		unsigned long kexec_flags)
 {
-	const char iomem[]= "/proc/iomem";
+	const char iomem[]= proc_iomem();
 	int memory_ranges = 0;
 	char line[MAX_LINE];
 	FILE *fp;

commit d91bf9213a6423257c78e0006182a43a363cdc37

arm: invalid initialisation of iomem in get_memory_ranges()
This fixes a regression introduced by me in
changeset 2b0088bb881680c5e8b062a9b0f0a6ef1b88d52c
"kexec-tools: [arm] use proc_iomem()".

My bad :-(

Signed-off-by: Simon Horman <horms@verge.net.au>

kexec/arch/arm/kexec-arm.c
@@ -26,7 +26,7 @@ static struct memory_range memory_range[MAX_MEMORY_RANGES];
 int get_memory_ranges(struct memory_range **range, int *ranges,
 		unsigned long kexec_flags)
 {
-	const char iomem[]= proc_iomem();
+	const char *iomem = proc_iomem();
 	int memory_ranges = 0;
 	char line[MAX_LINE];
 	FILE *fp;