7ace39c1253d04e5c5ffa99e1959dce0d20aaa20
Jamey Sharp <jamey@thetovacompany.com>
2008-05-15 22:34:23 -0700

    Update KEXEC_ARCH_* constants from Linux kernel headers.
    Signed-off-by: Jamey Sharp <jamey@thetovacompany.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- kexec/kexec-syscall.h
+++ kexec/kexec-syscall.h
@@ -82,8 +82,11 @@ static inline long kexec_reboot(void)
 #define KEXEC_ARCH_PPC     (20 << 16)
 #define KEXEC_ARCH_PPC64   (21 << 16)
 #define KEXEC_ARCH_IA_64   (50 << 16)
-#define KEXEC_ARCH_S390    (22 << 16)
 #define KEXEC_ARCH_ARM     (40 << 16)
+#define KEXEC_ARCH_S390    (22 << 16)
+#define KEXEC_ARCH_SH      (42 << 16)
+#define KEXEC_ARCH_MIPS_LE (10 << 16)
+#define KEXEC_ARCH_MIPS    ( 8 << 16)
 
 #define KEXEC_MAX_SEGMENTS 16
 
