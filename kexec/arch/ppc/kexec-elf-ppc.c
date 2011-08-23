horms/kexec-tools

commit 1ea7cfd435aba0e095886db367e9616271f56c28

ppc: Remove some unused parameters and variables
Signed-off-by: Simon Horman <horms@verge.net.au>

kexec/arch/ppc/kexec-elf-ppc.c
@@ -187,8 +187,6 @@ int elf_ppc_load(int argc, char **argv,	const char *buf, off_t len,
 	char *fixup_nodes[FIXUP_ENTRYS + 1];
 	int cur_fixup = 0;
 	int opt;
-	char *blob_buf = NULL;
-	off_t blob_size = 0;
 
 	command_line = NULL;
 	dtb = NULL;

kexec/arch/ppc/crashdump-powerpc.c
@@ -79,7 +79,6 @@ static int get_crash_memory_ranges(struct memory_range **range, int *ranges)
 	int memory_ranges = 0;
 	char device_tree[256] = "/proc/device-tree/";
 	char fname[256];
-	char buf[MAXBYTES];
 	DIR *dir, *dmem;
 	int fd;
 	struct dirent *dentry, *mentry;

kexec/arch/ppc/fixup_dtb.c
@@ -18,7 +18,7 @@
 const char proc_dts[] = "/proc/device-tree";
 
 #ifdef DEBUG
-static void print_fdt_reserve_regions(char *blob_buf)
+static void print_fdt_reserve_regions(void)
 {
 	int i, num;
 
@@ -135,7 +135,7 @@ static char *expand_buf(int minexpand, char *blob_buf, off_t *blob_size)
 	return blob_buf;
 }
 
-static void fixup_reserve_regions(struct kexec_info *info, char *blob_buf, off_t *blob_size)
+static void fixup_reserve_regions(struct kexec_info *info, char *blob_buf)
 {
 	int ret, i;
 	int nodeoffset;
@@ -204,7 +204,7 @@ static void fixup_reserve_regions(struct kexec_info *info, char *blob_buf, off_t
 out:	;
 
 #ifdef DEBUG
-	print_fdt_reserve_regions(blob_buf);
+	print_fdt_reserve_regions();
 #endif
 }
 
@@ -392,7 +392,7 @@ char *fixup_dtb_finalize(struct kexec_info *info, char *blob_buf, off_t *blob_si
 {
 	fixup_nodes(nodes);
 	fixup_cmdline(cmdline);
-	fixup_reserve_regions(info, blob_buf, blob_size);
+	fixup_reserve_regions(info, blob_buf);
 	fixup_memory(info, blob_buf);
 	fixup_initrd(blob_buf);
 	fixup_crashkernel(info, blob_buf);

commit fd3178774930cb55f9e9aa6553889e9d54b4500d

kexec:ppc: Fix compilation failure
commit 1ea7cfd435aba0e095886db367e9616271f56c28 broke the ppc build as it removes
the declaration for two variables in use by the elf_ppc_load(). This patch reverts
the particular hunk.

Signed-off-by: Suzuki K. Poulose <suzuki@in.ibm.com>
Reported-by: Josh Boyer <jwboyer@gmail.com>
Signed-off-by: Simon Horman <horms@verge.net.au>

kexec/arch/ppc/kexec-elf-ppc.c
@@ -187,6 +187,8 @@ int elf_ppc_load(int argc, char **argv,	const char *buf, off_t len,
 	char *fixup_nodes[FIXUP_ENTRYS + 1];
 	int cur_fixup = 0;
 	int opt;
+	char *blob_buf = NULL;
+	off_t blob_size = 0;
 
 	command_line = NULL;
 	dtb = NULL;