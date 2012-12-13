20f722ebafd0b639080e2ca8acb9cfad4df407c4
Daniel Mack <zonque@gmail.com>
2012-12-13 14:35:34 +0100

    kexec-zImage-arm: add code to support --command-line along with --dtb
    If --dtb is called together with --command-line, we need to modify the
    binary dtb buffer. Luckily, we have libfdt functions available, so this
    is straight forward.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Tested-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- kexec/arch/arm/kexec-zImage-arm.c
+++ kexec/arch/arm/kexec-zImage-arm.c
@@ -359,16 +359,54 @@ int zImage_arm_load(int argc, char **argv, const char *buf, off_t len,
 		 */
 		if (dtb_file) {
 			dtb_buf = slurp_file(dtb_file, &dtb_length);
+
+			if (fdt_check_header(dtb_buf) != 0) {
+				fprintf(stderr, "Invalid FDT buffer.\n");
+				return -1;
+			}
+
+			if (command_line) {
+				const char *node_name = "/chosen";
+				const char *prop_name = "bootargs";
+				int off;
+
+				/* check if a /choosen subnode already exists */
+				off = fdt_path_offset(dtb_buf, node_name);
+
+				if (off == -FDT_ERR_NOTFOUND) {
+					dtb_length = fdt_totalsize(dtb_buf)
+							+ strlen(node_name) + 1
+							+ sizeof(struct fdt_node_header)
+							+ FDT_TAGSIZE;
+					dtb_buf = xrealloc(dtb_buf, dtb_length);
+					fdt_set_totalsize(dtb_buf, dtb_length);
+					off = fdt_add_subnode(dtb_buf, off, node_name);
+				}
+
+				if (off < 0) {
+					fprintf(stderr, "FDT: Error adding %s node.\n", node_name);
+					return -1;
+				}
+
+				dtb_length = fdt_totalsize(dtb_buf)
+						+ strlen(prop_name)
+						+ strlen(command_line) + 1
+						+ sizeof(struct fdt_property);
+				dtb_buf = xrealloc(dtb_buf, dtb_length);
+				fdt_set_totalsize(dtb_buf, dtb_length);
+
+				if (fdt_setprop(dtb_buf, off, prop_name,
+						command_line, strlen(command_line) + 1) != 0) {
+					fprintf(stderr, "FDT: Error setting %s/%s property.\n",
+						node_name, prop_name);
+					return -1;
+				}
+			}
 		} else {
 			/*
 			 * Extract the DTB from /proc/device-tree.
 			 */
 			create_flatten_tree(&dtb_buf, &dtb_length, command_line);
-		}
-
-		if (fdt_check_header(dtb_buf) != 0) {
-			fprintf(stderr, "Invalid FDT buffer.\n");
-			return -1;
 		}
 
 		if (base + atag_offset + dtb_length > base + offset) {
