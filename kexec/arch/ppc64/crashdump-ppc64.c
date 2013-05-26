3016efb68309ceaecec10130594d2e929a3e2fb9
Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
2013-05-26 11:41:25 +0800

    kexec-tools: ppc64: Remove saved_max_mem
    saved_max_mem is used to calculate the amount of memory that the previous
    kernel used. And passed to the dump-capture kernel by kernel commandline
    parameter "savemaxmem=". But in the dump-capture kernel, we never use
    this parameter now, so remove saved_max_mem and don't add "savemaxmem="
    to new kernel commandline.
    
    Signed-off-by: Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- kexec/arch/ppc64/crashdump-ppc64.c
+++ kexec/arch/ppc64/crashdump-ppc64.c
@@ -69,17 +69,6 @@ static int crash_max_memory_ranges;
  */
 mem_rgns_t usablemem_rgns = {0, NULL};
 
-/*
- * To store the memory size of the first kernel and this value will be
- * passed to the second kernel as command line (savemaxmem=xM).
- * The second kernel will be calculated saved_max_pfn based on this
- * variable.
- * Since we are creating/using usable-memory property, there is no way
- * we can determine the RAM size unless parsing the device-tree/memoy@/reg
- * property in the kernel.
- */
-uint64_t saved_max_mem = 0;
-
 static unsigned long long cstart, cend;
 static int memory_ranges;
 
@@ -303,13 +292,6 @@ static int get_crash_memory_ranges(struct memory_range **range, int *ranges)
 		crash_memory_range[memory_ranges].start = cstart;
 		crash_memory_range[memory_ranges++].end = cend;
 	}
-	/*
-	 * Can not trust the memory regions order that we read from
-	 * device-tree. Hence, get the MAX end value.
-	 */
-	for (i = 0; i < memory_ranges; i++)
-		if (saved_max_mem < crash_memory_range[i].end)
-			saved_max_mem = crash_memory_range[i].end;
 
 	*range = crash_memory_range;
 	*ranges = memory_ranges;
@@ -446,7 +428,6 @@ int load_crashdump_segments(struct kexec_info *info, char* mod_cmdline,
 	 * read by flatten_device_tree and modified if required
 	 */
 	add_cmdline_param(mod_cmdline, elfcorehdr, " elfcorehdr=", "K");
-	add_cmdline_param(mod_cmdline, saved_max_mem, " savemaxmem=", "M");
 	return 0;
 }
 
