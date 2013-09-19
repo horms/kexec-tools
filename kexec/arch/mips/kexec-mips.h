6f8758a0b492016a7189a814f6c3d1525c6d3dcd
Geert Uytterhoeven <geert@linux-m68k.org>
2013-09-19 11:10:19 +0200

    cris/mips: Remove unused setup_simple_* declarations
    Presumably these had been copied from ppc.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- kexec/arch/mips/kexec-mips.h
+++ kexec/arch/mips/kexec-mips.h
@@ -6,13 +6,6 @@
 
 #define CORE_TYPE_ELF32 1
 #define CORE_TYPE_ELF64 2
-extern unsigned char setup_simple_start[];
-extern uint32_t setup_simple_size;
-
-extern struct {
-	uint32_t spr8;
-	uint32_t spr9;
-} setup_simple_regs;
 
 int elf_mips_probe(const char *buf, off_t len);
 int elf_mips_load(int argc, char **argv, const char *buf, off_t len,
