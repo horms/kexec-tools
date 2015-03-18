23a806695fa7f596397c5653ac832a2539211e67
Geoff Levand <geoff@infradead.org>
2015-03-18 23:04:03 +0000

    purgatory: Add printf type checking
    To better catch coding problems add stricter type checking to the
    purgatory printf routines.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- purgatory/include/purgatory.h
+++ purgatory/include/purgatory.h
@@ -2,8 +2,9 @@
 #define PURGATORY_H
 
 void putchar(int ch);
-void sprintf(char *buffer, const char *fmt, ...);
-void printf(const char *fmt, ...);
+void sprintf(char *buffer, const char *fmt, ...)
+	__attribute__ ((format (printf, 2, 3)));
+void printf(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 void setup_arch(void);
 void post_verification_setup_arch(void);
 
