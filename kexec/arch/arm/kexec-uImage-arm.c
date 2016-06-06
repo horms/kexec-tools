45251676e5620b32beafd4fd9db7566a89f58f7e
Russell King <rmk@arm.linux.org.uk>
2016-06-06 17:59:50 +0100

    arm: fix pointer signedness warning in kexec-uImage-arm.c
    kexec/arch/arm/kexec-uImage-arm.c: In function 'uImage_arm_probe':
    kexec/arch/arm/kexec-uImage-arm.c:14:2: warning: pointer targets in passing argument 1 of 'uImage_probe_kernel' differ in signedness [-Wpointer-sign]
    
    Reviewed-by: Pratyush Anand <panand@redhat.com>
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- kexec/arch/arm/kexec-uImage-arm.c
+++ kexec/arch/arm/kexec-uImage-arm.c
@@ -11,7 +11,8 @@
 
 int uImage_arm_probe(const char *buf, off_t len)
 {
-	return uImage_probe_kernel(buf, len, IH_ARCH_ARM);
+	return uImage_probe_kernel((const unsigned char *)buf, len,
+				   IH_ARCH_ARM);
 }
 
 int uImage_arm_load(int argc, char **argv, const char *buf, off_t len,
