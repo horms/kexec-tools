horms/kexec-tools

commit cedeee0a30075030632a18e419419371ddf3fca3

x86: Introduce helpers for getting RSDP address
On x86 RSDP is fundamental for booting the machine. When second kernel
is incapable of parsing the RSDP address (eg. kexec next kernel on an EFI
system with EFI service disabled), kexec should prepare the RSDP address
for second kernel.

Introduce helpers for getting RSDP from multiple sources, including boot
params and EFI firmware.

For legacy BIOS interface, there is no better way to find the RSDP address
rather than scanning the memory region and search for it, and this will
always be done by the kernel as a fallback, so this is no need to try to
get the RSDP address for that case.

Signed-off-by: Kairui Song <kasong@redhat.com>
Signed-off-by: Simon Horman <horms@verge.net.au>

kexec/arch/i386/kexec-x86-common.c
@@ -39,6 +39,7 @@
 #include "../../firmware_memmap.h"
 #include "../../crashdump.h"
 #include "kexec-x86.h"
+#include "x86-linux-setup.h"
 #include "../../kexec-xen.h"
 
 /* Used below but not present in (older?) xenctrl.h */
@@ -392,4 +393,46 @@ int get_memory_ranges(struct memory_range **range, int *ranges,
 	return ret;
 }
 
+static uint64_t bootparam_get_acpi_rsdp(void) {
+	uint64_t acpi_rsdp = 0;
+	off_t offset = offsetof(struct x86_linux_param_header, acpi_rsdp_addr);
 
+	if (get_bootparam(&acpi_rsdp, offset, sizeof(acpi_rsdp)))
+		return 0;
+
+	return acpi_rsdp;
+}
+
+static uint64_t efi_get_acpi_rsdp(void) {
+	FILE *fp;
+	char line[MAX_LINE], *s;
+	uint64_t acpi_rsdp = 0;
+
+	fp = fopen("/sys/firmware/efi/systab", "r");
+	if (!fp)
+		return acpi_rsdp;
+
+	while(fgets(line, sizeof(line), fp) != 0) {
+		/* ACPI20= always goes before ACPI= */
+		if ((strstr(line, "ACPI20=")) || (strstr(line, "ACPI="))) {
+			s = strchr(line, '=') + 1;
+			sscanf(s, "0x%lx", &acpi_rsdp);
+			break;
+		}
+	}
+	fclose(fp);
+
+	return acpi_rsdp;
+}
+
+uint64_t get_acpi_rsdp(void)
+{
+	uint64_t acpi_rsdp = 0;
+
+	acpi_rsdp = bootparam_get_acpi_rsdp();
+
+	if (!acpi_rsdp)
+		acpi_rsdp = efi_get_acpi_rsdp();
+
+	return acpi_rsdp;
+}

kexec/arch/i386/kexec-x86.h
@@ -86,4 +86,5 @@ int nbi_load(int argc, char **argv, const char *buf, off_t len,
 void nbi_usage(void);
 
 extern unsigned xen_e820_to_kexec_type(uint32_t type);
+extern uint64_t get_acpi_rsdp(void);
 #endif /* KEXEC_X86_H */

kexec/arch/i386/x86-linux-setup.c
@@ -123,7 +123,6 @@ void setup_linux_bootloader_parameters_high(
 	cmdline_ptr[cmdline_len - 1] = '\0';
 }
 
-static int get_bootparam(void *buf, off_t offset, size_t size);
 static int setup_linux_vesafb(struct x86_linux_param_header *real_mode)
 {
 	struct fb_fix_screeninfo fix;
@@ -460,7 +459,7 @@ char *find_mnt_by_type(char *type)
 	return mntdir;
 }
 
-static int get_bootparam(void *buf, off_t offset, size_t size)
+int get_bootparam(void *buf, off_t offset, size_t size)
 {
 	int data_file;
 	char *debugfs_mnt, *sysfs_mnt;

kexec/arch/i386/x86-linux-setup.h
@@ -21,6 +21,7 @@ static inline void setup_linux_bootloader_parameters(
 }
 void setup_linux_system_parameters(struct kexec_info *info,
 	struct x86_linux_param_header *real_mode);
+int get_bootparam(void *buf, off_t offset, size_t size);
 
 
 #define SETUP_BASE    0x90000

commit 7018e436cd342e2b03c0d13ff3ceae0ca6dd4db6

x86: re-order includes to avoid duplicate struct e820entry
xenctrl.h defines struct e820entry as:

	if defined(__i386__) || defined(__x86_64__)
	...
	#define E820_RAM	1
	...
	struct e820entry {
		uint64_t addr;
		uint64_t size;
		uint32_t type;
	} __attribute__((packed));
	...
	#endif

 $ dpkg-query -S /usr/include/xenctrl.h
 libxen-dev:amd64: /usr/include/xenctrl.h
 $  dpkg-query -W libxen-dev:amd64
 libxen-dev:amd64	4.8.5+shim4.10.2+xsa282-1+deb9u11

./include/x86/x86-linux.h defines struct e820entry as:

	#ifndef E820_RAM
	struct e820entry {
		uint64_t addr;  /* start of memory segment */
		uint64_t size;  /* size of memory segment */
		uint32_t type;  /* type of memory segment */
		#define E820_RAM    1
		...
	} __attribute__((packed));
	#endif

Since cedeee0a3007 ("x86: Introduce helpers for getting RSDP address")
./kexec/arch/i386/kexec-x86-common.c includes

	+#include "x86-linux-setup.h"
	 #include "../../kexec-xen.h"

When xenctrl.h is present the above results in:

 $ gcc
 ...
 In file included from kexec/arch/i386/../../kexec-xen.h:5:0,
                  from kexec/arch/i386/kexec-x86-common.c:43:
 /usr/include/xenctrl.h:1271:8: error: redefinition of 'struct e820entry'
  struct e820entry {
         ^~~~~~~~~

 In file included from kexec/arch/i386/x86-linux-setup.h:3:0,
                  from kexec/arch/i386/kexec-x86-common.c:42:
 ./include/x86/x86-linux.h:16:8: note: originally defined here
  struct e820entry {
         ^~~~~~~~~
 ...
 $ gcc --version | head -1
 gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516

To militate this this problem re-order the includes so that
x86-linux.h is included after xenctrl.h and thus
struct e820entry will only be defined once due to it
being devined conditionally in x86-linux.h.

In practice the definitions are the same so it should
not matter which is chosen.

It also seems rather unpleasent to me to need to play
with include ordering. Perhaps a better solution in the longer
term would be to rename the local definition of struct e820entry.

Fixes: cedeee0a3007 ("x86: Introduce helpers for getting RSDP address")

kexec/arch/i386/kexec-x86-common.c
@@ -38,9 +38,9 @@
 #include "../../kexec-syscall.h"
 #include "../../firmware_memmap.h"
 #include "../../crashdump.h"
+#include "../../kexec-xen.h"
 #include "kexec-x86.h"
 #include "x86-linux-setup.h"
-#include "../../kexec-xen.h"
 
 /* Used below but not present in (older?) xenctrl.h */
 #ifndef E820_PMEM